ec9c55197a6ad08972552eee28ee77e6934caf8c
Slu Yin <thinkways@outlook.com>
2020-06-17 10:37:49 +0800

    Update docker-ccaa.sh

--- docker-ccaa.sh
+++ docker-ccaa.sh
@@ -46,6 +46,7 @@ function install_aria2(){
 	#安装aria2静态编译版本，来源于https://github.com/q3aql/aria2-static-builds/
 	wget -c ${aria2_url}
 	tar jxvf aria2*.tar.bz2
+	rm aria2*.tar.bz2
 	cd aria2*
 	make install
 	cd
